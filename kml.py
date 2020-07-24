b8589cbc081dcd004190981bda6c9af12f902a63
Markeloen <hamedak1999@gmail.com>
2020-07-25 01:31:22 +0430

    version 2.0

--- kml.py
+++ kml.py
@@ -1,13 +1,58 @@
 import simplekml
 from polycircles import polycircles
+import xlrd
+import sys
 
-polycircle = polycircles.Polycircle(latitude=40.768085,
-                                    longitude=-73.981885,
-                                    radius=20000,
-                                    number_of_vertices=36)
-kml = simplekml.Kml()
-pol = kml.newpolygon(name="Columbus Circle, Manhattan",
-                                         outerboundaryis=polycircle.to_kml())
-pol.style.polystyle.color = \
-        simplekml.Color.changealphaint(200, simplekml.Color.green)
-kml.save("test_kml_polygon_3_manhattan.kml")
+def make_kml(tupple_lists, range, header):
+    kml = simplekml.Kml()
+    counter = 0
+    for tupple_list in tupple_lists:
+        counter+=1
+        if(tupple_list[3] <= 30000000 and tupple_list[3] > 20000000):
+            polycircle = polycircles.Polycircle(latitude=tupple_list[1],
+                                        longitude=tupple_list[2],
+                                        radius=range * 10000,
+                                        number_of_vertices=36)
+            pol = kml.newpolygon(name=tupple_list[0], outerboundaryis=polycircle.to_kml())
+            pol.style.polystyle.color = \
+                    simplekml.Color.changealphaint(200, simplekml.Color.red)
+            
+        elif(tupple_list[3] <= 20000000 and tupple_list[3] > 10000000):
+            
+            print(counter)
+            polycircle = polycircles.Polycircle(latitude=tupple_list[1],
+                                        longitude=tupple_list[2],
+                                        radius=range * 10000,
+                                        number_of_vertices=36)
+            pol = kml.newpolygon(name=tupple_list[0], outerboundaryis=polycircle.to_kml())
+            pol.style.polystyle.color = \
+                    simplekml.Color.changealphaint(200, simplekml.Color.yellow)
+        elif(tupple_list[3] <= 10000000 and tupple_list[3] > 1000000):
+            polycircle = polycircles.Polycircle(latitude=tupple_list[1],
+                                        longitude=tupple_list[2],
+                                        radius=range * 10000,
+                                        number_of_vertices=36)
+            pol = kml.newpolygon(name=tupple_list[0], outerboundaryis=polycircle.to_kml())
+            pol.style.polystyle.color = \
+                    simplekml.Color.changealphaint(200, simplekml.Color.orange)
+        elif(tupple_list[3] <= 1000000):
+            polycircle = polycircles.Polycircle(latitude=tupple_list[1],
+                                        longitude=tupple_list[2],
+                                        radius=range * 10000,
+                                        number_of_vertices=36)
+            pol = kml.newpolygon(name=tupple_list[0], outerboundaryis=polycircle.to_kml())
+            pol.style.polystyle.color = \
+                    simplekml.Color.changealphaint(200, simplekml.Color.green)
+        else:
+            polycircle = polycircles.Polycircle(latitude=tupple_list[1],
+                                        longitude=tupple_list[2],
+                                        radius=range * 1000,
+                                        number_of_vertices=36)
+            pol = kml.newpolygon(name=tupple_list[0], outerboundaryis=polycircle.to_kml())
+            pol.style.polystyle.color = \
+                    simplekml.Color.changealphaint(200, simplekml.Color.black)
+    kml.save(f"{header}.kml")
+
+
+
+
--- main.py
+++ main.py
@@ -1,66 +1,72 @@
 import xlrd
 import sys
-import random
 import simplekml
 from polycircles import polycircles
+from kml import make_kml
 
 file_path = (r"C:\Users\Administrator\Desktop\Communication_Network\worldcities.xlsx")
 
 wb = xlrd.open_workbook(file_path)
 sheet = wb.sheet_by_index(0)
 
-randomlist = []
-for i in range(sheet.nrows-1):
-    randomlist.append(random.randint(1,10))
 
-kml = simplekml.Kml()
+number_of_values = 2
 
-locations = []
-for i in range(sheet.nrows):
-    locations.append(sheet.cell_value(i , 0))
-lats = []
-for i in range(sheet.nrows):
-    lats.append(sheet.cell_value(i, 2))
-lngs = []
-for i in range(sheet.nrows):
-    lngs.append(sheet.cell_value(i, 3))
-loc_tupples = list(zip(locations,lats, lngs,randomlist))
-del loc_tupples[0]
-for loc_tupple in loc_tupples:
-    if(loc_tupple[3] <= 10 and loc_tupple[3] > 7.5):
-        polycircle = polycircles.Polycircle(latitude=loc_tupple[1],
-                                    longitude=loc_tupple[2],
-                                    radius=70000,
-                                    number_of_vertices=36)
-        pol = kml.newpolygon(name=loc_tupple[0], outerboundaryis=polycircle.to_kml())
-        pol.style.polystyle.color = \
-                simplekml.Color.changealphaint(200, simplekml.Color.green)
-    if(loc_tupple[3] <= 7.5 and loc_tupple[3] > 5):
-        polycircle = polycircles.Polycircle(latitude=loc_tupple[1],
-                                    longitude=loc_tupple[2],
-                                    radius=70000,
-                                    number_of_vertices=36)
-        pol = kml.newpolygon(name=loc_tupple[0], outerboundaryis=polycircle.to_kml())
-        pol.style.polystyle.color = \
-                simplekml.Color.changealphaint(200, simplekml.Color.yellow)
-    if(loc_tupple[3] <= 5 and loc_tupple[3] > 2.5):
-        polycircle = polycircles.Polycircle(latitude=loc_tupple[1],
-                                    longitude=loc_tupple[2],
-                                    radius=70000,
-                                    number_of_vertices=36)
-        pol = kml.newpolygon(name=loc_tupple[0], outerboundaryis=polycircle.to_kml())
-        pol.style.polystyle.color = \
-                simplekml.Color.changealphaint(200, simplekml.Color.orange)
-    if(loc_tupple[3] <= 2.5 and loc_tupple[3] > 0):
-        polycircle = polycircles.Polycircle(latitude=loc_tupple[1],
-                                    longitude=loc_tupple[2],
-                                    radius=70000,
-                                    number_of_vertices=36)
-        pol = kml.newpolygon(name=loc_tupple[0], outerboundaryis=polycircle.to_kml())
-        pol.style.polystyle.color = \
-                simplekml.Color.changealphaint(200, simplekml.Color.red)
 
-kml.save("world.kml")
+
+for j in range(number_of_values):
+    locations = []
+    for i in range(sheet.nrows):
+        locations.append(sheet.cell_value(i , 0))
+    lats = []
+    for i in range(sheet.nrows):
+        lats.append(sheet.cell_value(i, 1))
+    lngs = []
+    for i in range(sheet.nrows):
+        lngs.append(sheet.cell_value(i, 2))
+    values = []
+    for i in range(sheet.nrows):
+        values.append(sheet.cell_value(i, 3+j))
+    loc_tupples = list(zip(locations,lats, lngs, values))
+    headers = loc_tupples[0]
+    del loc_tupples[0]
+    make_kml(loc_tupples,7,headers[3])
+
+# for loc_tupple in loc_tupples:
+#     if(loc_tupple[3] <= 10 and loc_tupple[3] > 7.5):
+#         polycircle = polycircles.Polycircle(latitude=loc_tupple[1],
+#                                     longitude=loc_tupple[2],
+#                                     radius=70000,
+#                                     number_of_vertices=36)
+#         pol = kml.newpolygon(name=loc_tupple[0], outerboundaryis=polycircle.to_kml())
+#         pol.style.polystyle.color = \
+#                 simplekml.Color.changealphaint(200, simplekml.Color.green)
+#     if(loc_tupple[3] <= 7.5 and loc_tupple[3] > 5):
+#         polycircle = polycircles.Polycircle(latitude=loc_tupple[1],
+#                                     longitude=loc_tupple[2],
+#                                     radius=70000,
+#                                     number_of_vertices=36)
+#         pol = kml.newpolygon(name=loc_tupple[0], outerboundaryis=polycircle.to_kml())
+#         pol.style.polystyle.color = \
+#                 simplekml.Color.changealphaint(200, simplekml.Color.yellow)
+#     if(loc_tupple[3] <= 5 and loc_tupple[3] > 2.5):
+#         polycircle = polycircles.Polycircle(latitude=loc_tupple[1],
+#                                     longitude=loc_tupple[2],
+#                                     radius=70000,
+#                                     number_of_vertices=36)
+#         pol = kml.newpolygon(name=loc_tupple[0], outerboundaryis=polycircle.to_kml())
+#         pol.style.polystyle.color = \
+#                 simplekml.Color.changealphaint(200, simplekml.Color.orange)
+#     if(loc_tupple[3] <= 2.5 and loc_tupple[3] > 0):
+#         polycircle = polycircles.Polycircle(latitude=loc_tupple[1],
+#                                     longitude=loc_tupple[2],
+#                                     radius=70000,
+#                                     number_of_vertices=36)
+#         pol = kml.newpolygon(name=loc_tupple[0], outerboundaryis=polycircle.to_kml())
+#         pol.style.polystyle.color = \
+#                 simplekml.Color.changealphaint(200, simplekml.Color.red)
+
+# kml.save("world.kml")
 
 
 
